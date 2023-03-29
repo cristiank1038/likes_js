61df544dddfebe449c1bfcf1e0ecc4a5b96adb7e
Cristian Cortes <cristiank09@hotmail.com>
2023-03-29 13:25:43 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+/*
+function changeValue(event){
+    let plusValue = parseInt(valueNum.innerHTML);
+    plusValue += 1;
+    valueNum.innerHTML = plusValue;
+}
+
+let valueNum = document.querySelector('.likesValue');
+let likeButton = document.querySelectorAll('.likes');
+
+for(let i = 0; i < likeButton.length; i++){
+    likeButton[i].addEventListener('click', changeValue);
+}
+*/
+
+
+function changeValue1(){
+    let valueNum = document.querySelector('.likesValue1');
+    let plusValue = parseInt(valueNum.innerHTML);
+    plusValue += 1;
+    valueNum.innerHTML = plusValue;
+}
+
+function changeValue2(){
+    let valueNum = document.querySelector('.likesValue2');
+    let plusValue = parseInt(valueNum.innerHTML);
+    plusValue += 1;
+    valueNum.innerHTML = plusValue;
+}
+
+function changeValue3(){
+    let valueNum = document.querySelector('.likesValue3');
+    let plusValue = parseInt(valueNum.innerHTML);
+    plusValue += 1;
+    valueNum.innerHTML = plusValue;
+}
+
